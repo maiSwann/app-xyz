9b7b4e4762685d8e071116848de85a8d68a3728f
mailys <mailys.perez@epitech.eu>
2020-06-30 10:44:01 +0200

    initial xcode project

---
+++ app-xyz/app-xyz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-xyz
+//
+//  Created by Maïlys Perez on 30/06/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
