9a7ca4c021b5079436042b78d0460897b7bdc54c
mailys <mailys.perez@epitech.eu>
2020-06-30 10:52:30 +0200

    [ADD] frontend and autolayout

--- app-xyz/app-xyz/ViewController.swift
+++ app-xyz/app-xyz/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
