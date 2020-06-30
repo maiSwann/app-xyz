5c205e27dcb5e426104526a9d686b87bc07e754e
mailys <mailys.perez@epitech.eu>
2020-06-30 11:57:49 +0200

    [ADD] handling acceloremeter data

--- app-xyz/app-xyz/ViewController.swift
+++ app-xyz/app-xyz/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
@@ -14,8 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
     
+    var motionManager: CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabel)
+    }
+    
+    func updateLabel(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else {
+            return
+        }
+        let formater = NumberFormatter()
+        formater.minimumFractionDigits = 1
+        formater.maximumFractionDigits = 1
+        
+        let x = formater.string(for: accelerometerData.acceleration.x)!
+        let y = formater.string(for: accelerometerData.acceleration.y)!
+        let z = formater.string(for: accelerometerData.acceleration.z)!
+        
+        xLabel.text = "X: \(x)"
+        yLabel.text = "Y: \(y)"
+        zLabel.text = "Z: \(z)"
     }
 }
 
